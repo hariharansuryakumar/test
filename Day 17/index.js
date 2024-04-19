51dde1283ec95a13c17ece38fb189065a3b6d052
hariharansuryakumar <harithsurya26@gmailcom>
2024-04-19 23:27:22 +0530

    task 5

---
+++ Day 17/index.js
@@ -0,0 +1,9 @@
+const functionOne =() =>{
+    console.log("Hey,this function one!!");
+    functionTwo();
+    console.log("Hey,this is function Two");
+};
+const functionThree = () => {
+    console.log("Hey,this is function three");
+
+}
